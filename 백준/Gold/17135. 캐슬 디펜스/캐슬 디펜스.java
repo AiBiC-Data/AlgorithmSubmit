65519df2dee5c2e3ffc7cf85bbbd0f9365fa0d37
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-08-21 00:10:39 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 208 ms, Memory: 28544 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,91 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, M, D;
-    static int[][] matrix;
-    static int[] dx = {0, -1, 0};
-    static int[] dy = {-1, 0, 1};
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int N, M, D;
+	static int[][] arr;
+	static int[] dx = { 0, -1, 0 };
+	static int[] dy = { -1, 0, 1 };
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        M = scanner.nextInt();
-        D = scanner.nextInt();
-        matrix = new int[N][M];
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                matrix[i][j] = scanner.nextInt();
-            }
-        }
-        
-        int answer = 0;
-        List<int[]> archorPos = new ArrayList<>();
-        
-        for (int i = 0; i < M; i++) {
-            for (int j = i + 1; j < M; j++) {
-                for (int k = j + 1; k < M; k++) {
-                    archorPos.add(new int[]{i, j, k});
-                }
-            }
-        }
-        
-        for (int[] a : archorPos) {
-            answer = Math.max(answer, kill(a));
-        }
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
 
-        System.out.println(answer);
-    }
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-    static int kill(int[] archor) {
-        int[][] tempMatrix = new int[N][M];
+		int ans = 0;
+		List<int[]> where = new ArrayList<>();
 
-        for (int i = 0; i < N; i++) {
-            System.arraycopy(matrix[i], 0, tempMatrix[i], 0, M);
-        }
+		for (int i = 0; i < M; i++) {
+			for (int j = i + 1; j < M; j++) {
+				for (int k = j + 1; k < M; k++) {
+					where.add(new int[] { i, j, k });
+				}
+			}
+		}
 
-        int[][] killed = new int[N][M];
-        int result = 0;
+		for (int[] arc : where) {
+			ans = Math.max(ans, kill(arc));
+		}
 
-        for (int i = N - 1; i >= 0; i--) {
-            List<int[]> thisTurn = new ArrayList<>();
+		System.out.println(ans);
+	}
 
-            for (int ay : archor) {
-                Deque<int[]> dq = new ArrayDeque<>();
-                dq.add(new int[]{i, ay, 1});
+	static int kill(int[] archor) {
+		int[][] map = new int[N][M];
 
-                while (!dq.isEmpty()) {
-                    int[] current = dq.poll();
-                    int x = current[0];
-                    int y = current[1];
-                    int d = current[2];
+		for (int i = 0; i < N; i++) {
+			map[i] = Arrays.copyOf(arr[i], M);
+		}
 
-                    if (tempMatrix[x][y] == 1) {
-                        thisTurn.add(new int[]{x, y});
-                        if (killed[x][y] == 0) {
-                            killed[x][y] = 1;
-                            result += 1;
-                        }
-                        break;
-                    }
+		int[][] killed = new int[N][M];
+		int result = 0;
 
-                    if (d < D) {
-                        for (int di = 0; di < 3; di++) {
-                            int nx = x + dx[di];
-                            int ny = y + dy[di];
-                            if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
-                                dq.add(new int[]{nx, ny, d + 1});
-                            }
-                        }
-                    }
-                }
-            }
+		for (int i = N - 1; i >= 0; i--) {
+			List<int[]> thisTurn = new ArrayList<>();
 
-            for (int[] pos : thisTurn) {
-                tempMatrix[pos[0]][pos[1]] = 0;
-            }
-        }
+			for (int arc : archor) {
+				Deque<int[]> q = new ArrayDeque<>();
+				q.add(new int[] { i, arc, 1 });
 
-        return result;
-    }
+				while (!q.isEmpty()) {
+					int[] current = q.poll();
+					int x = current[0];
+					int y = current[1];
+					int d = current[2];
+
+					if (map[x][y] == 1) {
+						thisTurn.add(new int[] { x, y });
+						if (killed[x][y] == 0) {
+							killed[x][y] = 1;
+							result += 1;
+						}
+						break;
+					}
+
+					if (d < D) {
+						for (int di = 0; di < 3; di++) {
+							int nx = x + dx[di];
+							int ny = y + dy[di];
+							if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+								q.add(new int[] { nx, ny, d + 1 });
+							}
+						}
+					}
+				}
+			}
+
+			for (int[] t : thisTurn) {
+				map[t[0]][t[1]] = 0;
+			}
+		}
+		return result;
+	}
 }
