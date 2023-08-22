c3116f5987e118d093eba87a5ac9d905290163ba
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-08-22 22:46:28 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 5552 ms, Memory: 349544 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -0,0 +1,24 @@
+import collections, sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+def dfs(v):
+    for i in graph[v]:
+        if visited[i]==0:
+            visited[i]=1
+            dfs(i)
+            check[v][0]+=check[i][1]
+            check[v][1]+=min(check[i])
+            
+
+n = int(input())
+graph=collections.defaultdict(list)
+check =[[0,1] for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
+for _ in range(n-1):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited[1]=1
+dfs(1)
+print(min(check[1]))
