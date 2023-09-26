988ae43a52e281e0e009e409a76d4b873ab18825
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-09-27 00:36:49 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 148 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -11,7 +10,6 @@ public class Main {
 	static int N, M;
 	static char[][] map;
 	static int[][][] visited;
-	static ArrayList<int[]> endpoint;
 
 	static int[] dx = { 0, 0, -1, 1 };
 	static int[] dy = { -1, 1, 0, 0 };
@@ -21,7 +19,6 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		endpoint = new ArrayList<>();
 		int[] startpoint = new int[2];
 		map = new char[N][M];
 
@@ -30,15 +27,15 @@ public class Main {
 
 			for (int j = 0; j < M; j++) {
 				map[i][j] = str.charAt(j);
-				if (map[i][j] == '1') {
-					endpoint.add(new int[] { i, j });
-				} else if (map[i][j] == '0') {
+				if (map[i][j] == '0') {
 					startpoint[0] = i;
 					startpoint[1] = j;
 				}
 			}
 		}
+
 		visited = new int[N][M][64];
+
 		System.out.println(bfs(startpoint[0], startpoint[1]));
 	}
 
@@ -58,9 +55,9 @@ public class Main {
 				x = tmp[0];
 				y = tmp[1];
 				ori_key = tmp[2];
-				
+
 				if (map[x][y] == '1') {
-					return move-1;
+					return move - 1;
 				}
 
 				for (int j = 0; j < 4; j++) {
@@ -70,22 +67,24 @@ public class Main {
 
 					if (nx < 0 || ny < 0 || nx >= N || ny >= M || map[nx][ny] == '#' || visited[nx][ny][key] == 1)
 						continue;
-					
+
 					if (map[nx][ny] >= 'a' && map[nx][ny] <= 'f') {
 						visited[nx][ny][key |= 1 << (map[nx][ny] - 'a')] = 1;
 						q.add(new int[] { nx, ny, key |= 1 << (map[nx][ny] - 'a') });
-					}
-					else {
-					visited[nx][ny][key] = 1;
-					if (map[nx][ny] >= 'A' && map[nx][ny] <= 'F' && (key & (1 << (map[nx][ny] - 'A'))) == 0) {
-						continue;}
-					q.add(new int[] { nx, ny, key });
+					} else {
+						visited[nx][ny][key] = 1;
+						if (map[nx][ny] >= 'A' && map[nx][ny] <= 'F' && (key & (1 << (map[nx][ny] - 'A'))) == 0) {
+							continue;
+						}
+						q.add(new int[] { nx, ny, key });
 					}
 //					System.out.println(nx + " " + ny + " " + key + " "  + move);
 				}
+
 			}
 			move++;
 		}
 		return ans;
 	}
+
 }
