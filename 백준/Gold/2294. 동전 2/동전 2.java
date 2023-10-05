672b6b6f7c621c40db9ab451b0f4e3d2b5fd644c
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-10-05 09:31:31 +0900

    [Gold V] Title: 동전 2, Time: 156 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,K;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		ArrayList<Integer> coins = new ArrayList<>();
+		int[] dp = new int[K+1];
+		Arrays.fill(dp, 10001);
+		for (int i = 0; i < N; i++) {
+			coins.add(Integer.parseInt(br.readLine()));
+		}
+		dp[0]=0;
+		for(int coin: coins) {
+			for (int i = coin; i < K+1; i++) {
+				dp[i] = Math.min(dp[i], dp[i-coin]+1);
+			}
+		}
+		if(dp[K]==10001)System.out.println(-1);
+		else System.out.println(dp[K]);
+	}
+
+}
