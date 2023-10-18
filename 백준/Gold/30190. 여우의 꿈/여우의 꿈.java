39d263e1c557a3dee8ec2a283bdd79f47bb36271
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-10-18 21:37:37 +0900

    [Gold V] Title: 여우의 꿈, Time: 512 ms, Memory: 80600 KB -BaekjoonHub

--- 백준/Gold/30190. 여우의 꿈/여우의 꿈.java
+++ 백준/Gold/30190. 여우의 꿈/여우의 꿈.java
@@ -1,62 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static StringTokenizer stt;
-	static int K, middle, ans = 0;
+	static StringTokenizer st;
 	static final int MOD = 1000000007;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		stt = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(stt.nextToken());
-		K = Integer.parseInt(stt.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
-		int[] dp = new int[N];
 		if (N == 1) {
 			int i = Integer.parseInt(br.readLine());
-			if (K == i)
-				System.out.println(0);
-			else {
-				System.out.println(1);
+			if (K == i) {
+				sb.append(0);
+				System.out.println(sb.toString());
+			} else {
+				sb.append(1);
+				System.out.println(sb.toString());
 			}
 		} else {
+			int ans = 0;
+			int[] dp = new int[N];
 			dp[1] = 1;
+
 			for (int i = 2; i < N; i++) {
 				dp[i] = (2 * dp[i - 1] + 1) % MOD;
 			}
 
 			int[] panes = new int[N];
-			stt = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				panes[i] = Integer.parseInt(stt.nextToken());
+				panes[i] = Integer.parseInt(st.nextToken());
 			}
 
 			for (int i = N - 1; i > 0; i--) {
 				// 1. 현재 위치면 0 2. 다른 위치면 하노이탑 n-1번째 옮기기
 				if (K == panes[i])
 					continue;
-				ans += dp[i] + 1;
-				ans %= MOD;
+				ans = (ans + dp[i] + 1) % MOD;
 				K = 6 - K - panes[i];
 			}
 
 			// 마지막 값 계산
 			if (K == panes[0]) {
 			} else {
-				ans += 1;
-				ans %= MOD;
+				ans = (ans + 1) % MOD;
 			}
-
-			System.out.println(ans);
+			sb.append(ans);
+			System.out.println(sb.toString());
 		}
 	}
-	
 }
