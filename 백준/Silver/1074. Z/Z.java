1c5772ced21ca85f96bcd37fa3bb504e8d9d4aad
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-12-19 00:55:12 +0900

    Update Z.java

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -9,17 +9,17 @@ public class Main {
         int cnt = 0;
 
         while (N > 1) {
-            int size = (int) (Math.pow(2,N) / 2);
+            int size = (int) (Math.pow(2,N) / 2);    # 처음 분할할 갯수
             
-            if (r < size && c < size) {
+            if (r < size && c < size) {  # 2사분면
             	
-            } else if (r < size && c >= size) {
+            } else if (r < size && c >= size) {  # 1사분면
                 cnt += size * size;
                 c -= size;
-            } else if (r >= size && c < size) {
+            } else if (r >= size && c < size) {  # 4사분면
                 cnt += size * size * 2;
                 r -= size;
-            } else if (r >= size && c >= size) {
+            } else if (r >= size && c >= size) {  # 3사분면
                 cnt += size * size * 3;
                 r -= size;
                 c -= size;
@@ -38,3 +38,8 @@ public class Main {
         }
     }
 }
+
+# 분할정복은 while문으로 컨트롤 할거다.
+# 먼저 size 설정이 필요하다.
+# 그 다음 조건을 생각하며 분할을 시킨다.
+# 조건을 빠져 나오고 나서도 조건문을 통해 답을 구하는 경우도 있다.
