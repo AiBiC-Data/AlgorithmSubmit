be0bb0b9ca698a48829b4a995d4920f2a13b5dbd
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-12-19 01:34:10 +0900

    Update 외판원 순회 2.java

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -11,20 +11,20 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
-		nums = new int[N];
+		arr = new int[N][N];  # 비용 저장할 2차원 배열
+		nums = new int[N];	# 방문할 순서를 저장할 배열
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		visited = new boolean[N];
+		visited = new boolean[N];	# 방문 한 도시
 		ans = Integer.MAX_VALUE;
-		perm(0,0);
+		perm(0,0);	# TSP는 모두 순환 할 것이므로 순열로 줄을 세운다
 		System.out.println(ans);
 	}
-	static void perm(int dep, int flag) {
+	static void perm(int dep, int flag) {	# 비트마스킹을 이용한 순열 코드
 		if(dep==N) {
 			if(arr[nums[N-1]][nums[0]]>0) {
 				int cnt = 0;
@@ -42,3 +42,6 @@ public class Main {
 		}
 	}
 }
+# TSP를 인식한 순간 어차피 전부 방문해야 한다.
+# 순열을 이용해서 순서를 정하고 마지막에 계산하면 되는데
+# 시간을 조금이라도 줄이려면 NP사용하면 된다.
